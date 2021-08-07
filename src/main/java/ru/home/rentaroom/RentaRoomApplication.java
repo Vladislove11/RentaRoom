d261c1080355d4ae49c9ef35200ede13e86fa67d
VladDorofeev <vladislove11@gmail.com>
2021-08-07 10:31:55 +0300

    Initial commit

---
+++ src/main/java/ru/home/rentaroom/RentaRoomApplication.java
@@ -0,0 +1,13 @@
+package ru.home.rentaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentaRoomApplication.class, args);
+    }
+
+}
